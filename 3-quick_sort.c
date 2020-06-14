4df1eba72874eee66f93fde67e16a20925ce9638
daniloromero <1601@holbertonschool.com>
2020-06-14 01:10:09 +0000

    1st version

---
+++ 3-quick_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+ *
+ *
+ */
+void quick_sort(int *array, size_t size)
+{
+
+	if (array == NULL || size <= 1)
+		return;
+	partition(array, size, array, size);
+}
+
+void partition(int *array, size_t size, int *vector, size_t len)
+{
+	
+	size_t low = 0, high = len - 1, i, j;
+	int pivot = 0, swap = 0;
+
+	if (len <= 1)
+		return;
+	i = low - 1, j = low;
+	pivot = vector[high];
+	for (; j < high; j++)
+	{
+		if (vector[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap = vector[i];
+				vector[i] = vector[j];
+				vector[j] = swap;
+				print_array(array, size);
+			}
+		}
+	}
+	if (pivot < vector[i + 1])
+	{
+		swap = vector[i + 1];
+		vector[i + 1] = vector[high];
+		vector[high] = swap;
+		print_array(array, size);
+	}
+
+	partition(array, size, vector, (i + 1));
+	partition(array, size, &vector[i + 2], (high - (i + 1)));
+}
