b542c6b5fbe99768725a19dbbda751a4a56884db
SneyderHOL <1507@holbertonschool.com>
2020-06-14 15:28:33 -0500

    [UPDATE]: betty style

--- sort.h
+++ sort.h
@@ -12,9 +12,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void partition(int *array, size_t size, int *vector, size_t len);
