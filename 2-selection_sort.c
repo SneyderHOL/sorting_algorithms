c512ea51e4504e117bc8aad82352a368ca0397f2
SneyderHOL <1507@holbertonschool.com>
2020-06-14 17:50:01 -0500

    [UPDATE]: minor assignent

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,23 +8,31 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t current = 0, next = 0, aux = 0;
+	size_t current = 0, next = 0, minor = 0;
+	int aux = 0;
 
 	if (array == NULL && size <= 1)
 		return;
 	for (current = 0; current < size - 1; current++)
 	{
+		minor = current;
 		next = current + 1;
+		aux = array[current];
 		while (next < size)
 		{
-			if (array[current] > array[next])
+			if (aux > array[next])
 			{
-				aux = array[current];
-				array[current] = array[next];
-				array[next] = aux;
+				minor = next;
+				aux = array[next];
 			}
 			next++;
 		}
-		print_array(array, size);
+		if (minor != current)
+		{
+			aux = array[current];
+			array[current] = array[minor];
+			array[minor] = aux;
+			print_array(array, size);
+		}
 	}
 }
