23c57bc91588d2d83b3ef2cd1c256fa5f4caf87c
daniloromero <1601@holbertonschool.com>
2020-06-14 21:52:10 +0000

    Split in shorter fucntions

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,8 +1,9 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - 
- *
+ * insertion_sort_list - sorts doubly linked list of integers ascending order
+ * @list: double linked list
+ * Return: void
  */
 
 void insertion_sort_list(listint_t **list)
@@ -18,41 +19,63 @@ void insertion_sort_list(listint_t **list)
 		if (check->n > check->next->n)
 		{
 			aux = check->next;
-			aux->prev = check->prev;
-			check->next = aux->next;
-			if (aux->next != NULL)
-				aux->next->prev = check;
-			check->prev = aux;
-			aux->next = check;
-			aux->prev->next = aux;
-			if (check == *list)
-				*list = aux;
-			back = *list;	
-			print_list(back);
+			swap_forward(list, check, aux);
 			while (aux->prev != NULL)
 			{
 				if (aux->n < aux->prev->n)
 				{
 					back = aux->prev;
-					aux->prev = back->prev;
-					back->next = aux->next;
-					aux->next->prev = back;
-					aux->next = back;
-					back->prev = aux;
-					if (aux->prev != NULL)
-						aux->prev->next = aux;
-					if (back == *list)
-						*list = aux;
-					back = *list;
-					print_list(back);
+					swap_backwards(list, back, aux);
 				}
 				else
-					break;				
+					break;
 			}
-		
+
 		}
 		else
 			check = check->next;
-	}	
+}
 
+}
+
+/**
+ *swap_forward - moves node forward
+ *@list: double linked list to be sorted
+ *@check: element to start comparison
+ *@aux: element to compare in list
+ *Return: void
+ */
+void swap_forward(listint_t **list, listint_t *check, listint_t *aux)
+{
+	aux->prev = check->prev;
+	check->next = aux->next;
+	if (aux->next != NULL)
+		aux->next->prev = check;
+	check->prev = aux;
+	aux->next = check;
+	aux->prev->next = aux;
+	if (check == *list)
+		*list = aux;
+	print_list(*list);
+}
+
+/**
+ *swap_backwards - moves
+ *@list: double linked list to be sorted
+ *@back: element to start comparison
+ *@aux: element to compare in list
+ *Return: void
+ */
+void swap_backwards(listint_t **list, listint_t *back, listint_t *aux)
+{
+	aux->prev = back->prev;
+	back->next = aux->next;
+	aux->next->prev = back;
+	aux->next = back;
+	back->prev = aux;
+	if (aux->prev != NULL)
+		aux->prev->next = aux;
+	if (back == *list)
+		*list = aux;
+		print_list(*list);
 }
