5f6643fa2ddc8241f1109a08c31ae7f816c0f153
daniloromero <1601@holbertonschool.com>
2020-06-17 01:43:06 +0000

    [UPDATE] Condition for interval > size

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,26 +8,28 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	int inter = 0, swap, close, far;
+	int inter = 1, swaped = 0, close = 0, far = 0;
 
 	if (array == NULL && size <= 1)
 		return;
 
-	while (inter < (int)size / 3)
+	while (inter < (int)size)
 		inter = (inter * 3) + 1;
+	if (inter >= (int)size)
+		inter = (inter - 1) / 3;
 
 	while (inter > 0)
 	{
 		for (far = inter; far < (int)size; far++)
 		{
-			swap = array[far];
+			swaped = array[far];
 			close = far;
-			while (close > inter - 1 && array[close - inter] > swap)
+			while (close > inter - 1 && array[close - inter] > swaped)
 			{
 				array[close] = array[close - inter];
 				close = close - inter;
 			}
-			array[close] = swap;
+			array[close] = swaped;
 		}
 		inter = (inter - 1) / 3;
 		print_array(array, size);
